e5e0d1cf0a22d7888948b5270630f4530dabdff7
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2025-02-11 00:50:38 +0530

    Add Nunito font, update styles, and implement new UI components

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,3 +1,19 @@
+import logo from '../../assets/images/myFoodie-logo.png';
+import Image from 'next/image';
+
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    const handleClick = () => {
+        console.log('Button clicked!');
+    }
+    return(
+        <>
+         <h1 className="font-nunito text-7xl font-extralight text-theme_green-500">Hello, Next.js!</h1>
+         <Image src={logo} alt="MyFoodie Logo"  className='w-36 h-auto pl-10'/>
+       
+        
+
+         
+         </>
+        )
 }
